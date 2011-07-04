c9e8fad9c377c52354e4a2eefc32b8e0723b5fe0
Lauren <lauren.tsung@yahoo.com>
2011-07-04 01:52:58 -0700

    Scraping titles for one of my flickr sets via Python-YQL

--- Python_YQL/Flickr_Python/flickrset.py
+++ Python_YQL/Flickr_Python/flickrset.py
@@ -12,7 +12,7 @@ import simplejson
 #  Date:   3 July 2011
 #  
   
-url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photosets.photos%20where%20photoset_id%3D'72157625239647550'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photosets.photos%20where%20photoset_id%3D'72157625114818289'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
 
 result = urllib2.urlopen(url).read()
 
@@ -21,4 +21,4 @@ data = simplejson.loads(result)
 total_photos = len(data['query']['results']['photo'])
 
 for i in range (0, total_photos):
-	print data['query']['results']['photo'][i]['id'] 
+	print data['query']['results']['photo'][i]['title'] 
